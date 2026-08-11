0695555dec45cb42b8d96435050af55753f4f990
leopettersen <leonardopettersen465@yahoo.com>
2026-08-11 19:33:51 -0300

    Aula 1 - RestApi

---
+++ RestAPI/src/main/java/com/example/RestAPI/Controller.java
@@ -0,0 +1,18 @@
+package com.example.RestAPI;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    // Exemplo de endpoint usando Spring Boot
+    @GetMapping("/test")
+    public String helloWorld(){
+        return "Essa é minha primeira API REST";
+    }
+
+    @GetMapping("/diaw")
+    public String diaw() {
+        return "Esse é meu segundo endpoint.";
+    }
+}
