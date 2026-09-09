7ed797beb77c4568556c55f67d4ab5c969d4de67
leopettersen <leonardopettersen465@yahoo.com>
2026-09-08 21:11:40 -0300

    LoginPUC v3

--- PROJETOS/SpringBoot/LoginPUC/src/main/java/com/example/LoginPUC/LoginPucApplication.java
+++ PROJETOS/SpringBoot/LoginPUC/src/main/java/com/example/LoginPUC/LoginPucApplication.java
@@ -3,7 +3,7 @@ package com.example.LoginPUC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example"})
 public class LoginPucApplication {
 
 	public static void main(String[] args) {
