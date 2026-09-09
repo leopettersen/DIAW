6996e8c8e5653b1c0666b6cdd7fd2a0bace071bd
leopettersen <leonardopettersen465@yahoo.com>
2026-09-08 21:11:40 -0300

    LoginPUC v3

--- PROJETOS/SpringBoot/LoginPUC/src/main/java/com/example/LoginPUC/controller/SecureLoginController.java
+++ PROJETOS/SpringBoot/LoginPUC/src/main/java/com/example/LoginPUC/controller/SecureLoginController.java
@@ -5,11 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-// @RestController -> apenas Back-end -> API Rest
-// @Controller -> também com Front-end -> HTML, CSS, JS, Thymeleaf
-
 @Controller
-public class LoginPucController {
+public class SecureLoginController {
 
     @GetMapping("/login")
     public String login() {
@@ -17,16 +14,24 @@ public class LoginPucController {
     }
 
     @GetMapping("/home")
-    public String home() { return "home"; }
+    public String home() {
+        return "home";
+    }
 
     @GetMapping("/error")
-    public String error() { return "error"; }
+    public String error() {
+        return "error";
+    }
 
     @GetMapping("/admin")
-    public String admin() { return "admin"; }
+    public String admin() {
+        return "admin";
+    }
 
     @GetMapping("/register")
-    public String register() { return "register"; }
+    public String register() {
+        return "register";
+    }
 
     @PostMapping("/register")
     public String handleRegister(
@@ -42,22 +47,24 @@ public class LoginPucController {
         // userService.saveUser(new User(nome, email, cpf, rg, endereco, instituicao, senha));
 
         // Redirecionar ou exibir uma mensagem de sucesso
-        System.out.println("Registro: Redirecionando para a página de login.");
+        System.out.println("Registro: Redirecionado para a página de login.");
         return "redirect:/login"; // Após o registro, redirecionar para a página de login
     }
 
     @GetMapping("/recoverpassword")
-    public String recoverpassword() { return "recoverpassword"; }
+    public String recoverpassword() {
+        return "recoverpassword";
+    }
 
     @PostMapping("/recoverpassword")
-    public String handleRecoverpassword(
+    public String handleRecoverPassword(
             @RequestParam("email") String email) {
 
         // Aqui você pode adicionar lógica para recuperar a senha.
         // userService.recoverPassword(email);
 
         // Redirecionar ou exibir uma mensagem de sucesso
-        System.out.println("Recuperação de E-mail: Redirecionando para a página de login.");
+        System.out.println("Recuperação de E-mail: Redirecionado para a página de login.");
         return "redirect:/login"; // Após a recuperação de senha, redirecionar para a página de login
     }
 }
