5c9542fc6154f6576795eab5cd26dbddf3fe31fc
leopettersen <leonardopettersen465@yahoo.com>
2026-09-14 22:33:56 -0300

    API de estações de rádio em BH

---
+++ PROJETOS/SpringBoot/RadioBrowserAPI/src/main/java/com/example/RadioBrowserAPI/RadioBrowserApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.RadioBrowserAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadioBrowserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadioBrowserApiApplication.class, args);
+	}
+
+}
